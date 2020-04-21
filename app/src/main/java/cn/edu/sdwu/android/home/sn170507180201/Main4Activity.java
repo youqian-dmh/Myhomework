26c990b1a131f13d3341bf3f6caf83bb0174e80d
youqian-dmh <605736963@qq.com>
2020-04-21 11:08:53 +0800

    修改图标名称

---
+++ app/src/main/java/cn/edu/sdwu/android/home/sn170507180201/Main4Activity.java
@@ -0,0 +1,12 @@
+package cn.edu.sdwu.android.home.sn170507180201;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
